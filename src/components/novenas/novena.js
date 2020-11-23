7aaec0d546cb830ca63d68af7d1072962b9e52c8
Danilo Santos <danilosantosalvarado@gmail.com>
2020-11-23 09:53:29 -0500

    ajustes v4

--- src/components/novenas/novena.js
+++ src/components/novenas/novena.js
@@ -48,10 +48,10 @@ export const StyleNovena = styled.div`
     border-style: solid;
     border-width: 10px 10px 10px 10px;
     border-image: url("${borderRed}") 9 9 9 9 repeat repeat;
-    background: radial-gradient(#FF1A1A, #AE2424 78%);
-    background-position: center;
+    background-image: url("${chispas}"), radial-gradient(#FF1A1A, #AE2424 78%);
+    background-position: center, center;
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: 182%, cover;
   }
   .day-home-wrap:before {
     content: '';
@@ -111,9 +111,7 @@ export const StyleNovena = styled.div`
     border-right: 2px solid #fff;
     border-radius: 3px;
   }
-
 `;
-
 export const PortadaDay = styled.div`
   background: url("${mascara}") center/cover no-repeat #AE2424;
   padding: 1em 1.1em;
@@ -226,7 +224,7 @@ export const Oracion = styled.div`
     display: block;
     width: 17px;
     height: 18px;
-    background: url("${point}") center left/100% no-repeat;
+    background: url("${point}") top left/100% no-repeat;
     margin-left: 10px;
   }
   .img-oraci img{
@@ -235,7 +233,8 @@ export const Oracion = styled.div`
   }
   .conte-text {
     height: 60%;
-    overflow: hidden;
+    overflow-x: hidden;
+    overflow-y: scroll;
     position: relative;
   }
   .conte-text:after {
@@ -270,5 +269,99 @@ export const Oracion = styled.div`
     width: 100%;
     height: 7.4em;
     background: url("${oracionbottom}") center top/115% no-repeat;
+  }
+`;
+export const MenuOraciones = styled.div`
+  background: #027334;
+  padding: 1em 1.1em;
+  z-index: 2;
+  position: relative;
+  @media (max-width: 768px){
+    min-height: 100vh
+  }
+  .close {
+    display: inline-block;
+    float: right;
+    text-decoration: none;
+    margin: .5em;
+  }
+  .navbar {
+    margin-bottom: 1em;
+    clear: both;
+    display: block;
+    overflow: hidden;
+  }
+  .img-comparte img {
+    width: 80%;
+  }
+  .grip-oracio {
+    height: calc(100vh - 2em);
+    padding: 13% 0;
+    text-align: center;
+    border-style: solid;
+    border-width: 10px 10px 10px 10px;
+    border-image: url("${borderRed}") 9 9 9 9 repeat repeat;
+    background-image: url("${chispas}"), radial-gradient(#FF1A1A, #AE2424 78%);
+    background-position: center, center;
+    background-repeat: no-repeat;
+    background-size: 182%, cover;
+  }
+  .grip-oracio:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4em;
+    background: url("${dayTop}") center bottom/100% no-repeat;
+  }
+  .grip-oracio:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 4.4em;
+    background: url("${dayBottom}") center top/119% no-repeat;
+  }
+  .comparte a,
+  .menu-oracion a {
+    min-width: calc(100% - 3em);
+    display: inline-block;
+    font-size: 0.8rem;
+    color: #08B761;
+    text-transform: capitalize;
+    text-decoration: none;
+    border-radius: 20px;
+    padding: .5em 2.5em;
+    margin-bottom: 7%;
+    background: #fff;
+    box-shadow: 0 3px 3px rgb(0 0 0 / 14%);
+  }
+  .comparte p {
+    color: white;
+    margin-bottom: 1em;
+  }
+  .arrow {
+    position: absolute;
+    bottom: 1.8em;
+    left: 50%;
+    transform: rotate(45deg) translateX(-50%);
+    width: 14px;
+    height: 14px;
+    border-bottom: 2px solid #fff;
+    border-right: 2px solid #fff;
+    border-radius: 3px;
+  }
+  .arrow:before {
+    content:'' ;
+    position absolute;
+    top: -5px;
+    left: -5px;
+    width: 14px;
+    height: 14px;
+    border-bottom: 2px solid #fff;
+    border-right: 2px solid #fff;
+    border-radius: 3px;
   }
 `;
