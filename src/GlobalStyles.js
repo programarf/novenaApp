40302df151e96b99f5cb7254da495be430f72af1
Danilo Santos <danilosantosalvarado@gmail.com>
2020-11-26 19:12:03 -0500

    style resetas v14

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -37,6 +37,8 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 300;
     font-display: swap;
     src: url("https://use.typekit.net/af/620bf8/00000000000000000000e7fe/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("woff2"),
+    url("https://use.typekit.net/af/620bf8/00000000000000000000e7fe/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("woff"),
+    url("https://use.typekit.net/af/620bf8/00000000000000000000e7fe/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n3&v=3") format("opentype"),
     url('${MuseoSans300Otf}') format("opentype"),
     url('${MuseoSans300Woff}') format('woff'),
     url('${MuseoSans300Woff2}') format('woff2');
@@ -47,6 +49,8 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 500;
     font-display: swap;
     src: url("https://use.typekit.net/af/a28b50/00000000000000000000e803/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n5&v=3") format("woff2"),
+    url("https://use.typekit.net/af/c2d3de/00000000000000000000e804/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i5&v=3") format("woff"),
+    url("https://use.typekit.net/af/c2d3de/00000000000000000000e804/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i5&v=3") format("opentype"),
     url('${MuseoSans500Otf}') format("opentype"),
     url('${MuseoSans500Woff}') format('woff'),
     url('${MuseoSans500Woff2}') format('woff2');
